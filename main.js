19c282c2ff9a5195c133598294745cbe1c6af1c5
652175qwasZX <andreidiving@gmail.com>
2020-09-27 00:23:59 +0800

    lesson17

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Lesson 15
 
-var myNum = 10;
+/*var myNum = 10;
 var myStr = 'строка';
 var myBool = true;
 var myArr = [1, 2, 3, 4, 5];
@@ -70,7 +70,7 @@ console.log(myStr.toLowerCase());
 
 //Lesson 16
 
-var userObj = {
+/*var userObj = {
     firstName: 'Andrei',
     lastName: 'Kramarenko',
     age: 37,
@@ -89,9 +89,9 @@ console.log(defUpperStr());
 
 
 function evenFn(n) {
-    let arr = [];
+    var arr = [];
   
-    for (let i = 1; i <= n; i++) if (i % 2 === 0) arr.push(i);
+    for (var i = 1; i <= n; i++) if (i % 2 === 0) arr.push(i);
   
     return arr;
   }
@@ -101,7 +101,7 @@ function evenFn(n) {
 
 
 function weekFn(cond) {
-  let str = '';
+  var str = '';
 
   switch (cond) {
     case 1:
@@ -165,8 +165,8 @@ console.log('130 :', ageClassification(130));
 
 
 function oddFn(n) {
-    let arr = [];
-    let i = 0;
+    var arr = [];
+    var i = 0;
   
     while (i++ < n) if (i % 2 !== 0) arr.push(i);
   
@@ -195,4 +195,180 @@ function oddFn(n) {
   console.log(mainFunc(2, 5, cbRandom)); 
 console.log(mainFunc(2, 5, cbPow)); 
 console.log(mainFunc(2, 5, cbAdd)); 
-console.log(mainFunc(2, 5, 'not a func')); 
+console.log(mainFunc(2, 5, 'not a func'));*/ 
+
+//lesson 17
+
+//1
+
+var counter = (function () {
+  var count = 0;
+
+  return function (num) {
+    count = num === undefined ? count : num;
+    return count++;
+  };
+}());
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter(100));
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter(500));
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter(0));
+console.log(counter());
+console.log(counter());
+
+//2
+
+var counting = (function () {
+  var count = 0;
+
+  return {
+    value(num) {
+      if (num !== undefined) count = num;
+      return count;
+    },
+    decrement() {
+      count--;
+    },
+    increment() {
+      count++;
+    }
+  };
+}());
+
+console.log(counting.value()); 
+counting.increment();
+counting.increment();
+counting.increment();
+console.log(counting.value()); 
+counting.decrement();
+counting.decrement();
+console.log(counting.value());
+console.log(counting.value(100)); 
+counting.decrement();
+console.log(counting.value()); 
+console.log(counting.value(200)); 
+counting.increment();
+console.log(counting.value());
+
+//3
+
+var myPrint = (a, b, res) => `${a}^${b}=${res}`;
+var myPow = (a, b, callback) => {
+  var pow = (x, n) => {
+    if (n !== 1) return x *= pow(x, n - 1);
+
+    return x;
+  };
+
+  return callback(a, b, pow(a, b));
+};
+
+console.log(myPow(3, 4, myPrint)); 
+console.log(myPow(2, 3, myPrint));
+
+//4-5-6
+
+
+
+function fullInfo() {
+  return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`;
+}
+
+var yearNow = new Date().getFullYear();
+var car = {
+  engine: 2000,
+  model: 'Lacetti',
+  name: 'Chevrovar',
+  year: 2010,
+  info: fullInfo,
+  get used() {
+    return this.year !== yearNow ? 'used' : 'new';
+  },
+  set used(value) {
+    if (value === 'new' && this.year < yearNow) this.year = yearNow;
+  }
+};
+var car2 = {
+  engine: 5000,
+  model: 'FX50 AWD',
+  name: 'Infinite',
+  year: 2019,
+  info: fullInfo,
+  get used() {
+    return yearNow - this.year ? 'used' : 'new';
+  },
+  set used(value) {
+    if (value === 'new' && this.year < yearNow) this.year = yearNow;
+  }
+};
+
+console.log(car.info());
+car.used = 'new';
+console.log(car.info()); 
+car.used = 'used';
+console.log(car.info()); 
+console.log(car2.info()); 
+car.used = 'used';
+console.log(car2.info()); 
+
+//7
+
+var list = [12, 23, 100, 34, 56, 9, 233];
+var myMax = (arg) => Math.max.apply(Math, arg);
+
+console.log(myMax(list));
+
+//8
+
+function myMul(a, b) {
+  return a * b;
+};
+
+var myDouble = myMul.bind(null, 2); 
+
+console.log(myDouble(3)); 
+console.log(myDouble(4)); 
+console.log(myDouble(5)); 
+
+var myTriple = myMul.bind(null, 3); 
+console.log(myTriple(3)); 
+console.log(myTriple(4)); 
+console.log(myTriple(5)); 
+
+//9
+
+var notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
+var notUniqStrings = [
+  'Bob',
+  'Kate',
+  'Jhon',
+  'Tom',
+  'Jhon',
+  'Kate',
+  'Tom',
+  'Bob',
+  'Jhon',
+  'Tom'
+];
+
+var myUniq = (arr) => {
+  var set = new Set();
+
+  arr.forEach((val) => {
+    set.add(val);
+  });
+
+  return set;
+};
+
+console.log(myUniq(notUniqNums));
+console.log(myUniq(notUniqStrings));
